277e0e067498b02e85e25a428237d6b7b142f0f0
Andrew Scales <code@Ambiguous-MacBook-Pro.local>
2017-03-23 08:27:07 -0400

    updates for production

--- header.php
+++ header.php
@@ -22,10 +22,10 @@
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'usra' ); ?></a>
-
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
             <div class="site-custom-image"><?php the_header_image_tag(); ?></div>
+             <div class="us-flag"></div>
 			<?php
 			if ( is_front_page() && is_home() ) : ?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
@@ -39,12 +39,15 @@
             <div class="site-description-container"><p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p></div>
 			<?php
 			endif; ?>
+            
+           
 		</div><!-- .site-branding -->
-
+        
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'usra' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
+        
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
